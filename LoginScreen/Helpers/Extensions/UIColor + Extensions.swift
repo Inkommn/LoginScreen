c89aab73dbc84a0501ebf6eb6e6efea1ca0ed3d8
Shamkhan Mutuskhanov <inkomman@gmail.com>
2023-06-25 19:31:09 +0300

    update

--- LoginScreen/Helpers/Extensions/UIColor + Extensions.swift
+++ LoginScreen/Helpers/Extensions/UIColor + Extensions.swift
@@ -13,6 +13,6 @@ extension UIColor {
     }
     
     static var highlightedColor: UIColor {
-        #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     }
 }
